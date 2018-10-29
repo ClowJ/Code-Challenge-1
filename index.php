3ed55a9baa6ce1d203b8d819e8d75b9c7d408e8f
JOOJAE <clowj78@gmail.com>
2018-10-29 12:32:36 +1100

    unit test for btn action

--- index.php
+++ index.php
@@ -17,16 +17,42 @@
     </head>
 	<body>
         <div class="container">
-            <div class="card">
+            <div class="card my-3 display">
                 <div class="card-body">
                     <h3 class="card-title">Welcome to Code Challenge</h3>
+                    <p class="card-text">Please click Place below to place your packman somewhere.</p>
+                </div>
+            </div>
+            <div class="card form">
+                <div class="card-body">
+                    <div class="form-group place">
+                        <button class="btn btn-primary">PLACE</button>
+                    </div>
+                    <div class="form-group move">
+                        <button class="btn btn-info" id="move">MOVE</button>
+                        <button class="btn btn-info" id="left">LEFT</button>
+                        <button class="btn btn-info" id="right">RIGHT</button>
+                    </div>
+                    <div class="form-group report">
+                        <button class="btn btn-warning">REPORT</button>
+                    </div>
                 </div>
             </div>
         </div>
     </body>
     <script type="text/javascript">
         $(document).ready(function() { 
-        
+            $('.place .btn').on('click', function(e){
+                $('.card.display .card-body').append('<p class="card-text">CLICK place BTN</p>');
+            });
+
+            $('.move .btn').on('click', function(e){
+                $('.card.display .card-body').append('<p class="card-text">CLICK '+$(e.target).prop('id')+' BTN</p>');
+            });
+
+            $('.report .btn').on('click', function(e){
+                $('.card.display .card-body').append('<p class="card-text">CLICK report BTN</p>');
+            });
         });
     </script>
 </html>
